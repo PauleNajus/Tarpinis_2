817b6cbb8b013443d82a6df6bb9f8aa9f2e04d52
Lukas <lukaspabilionis@gmail.com>
2024-07-03 15:57:16 +0300

    deploy script v2

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "database.db")}'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f'sqlite:///{os.path.join(basedir, "database.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
